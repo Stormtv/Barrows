5d2bb7ff9537f338c61bf1da233472ebfbc68aa6
Ruben <ruben.amendoeira@gmail.com>
2013-08-01 04:01:46 +0100

    Fixed bug in getAnswer()

--- methods/TunnelPuzzle.java
+++ methods/TunnelPuzzle.java
@@ -19,7 +19,7 @@ public class TunnelPuzzle {
 
 	private static RSInterface getAnswer() {
 		if (isPuzzleScreenOpen()) {
-			for (int i = 1; i < 9; i++) {
+			for (int i = 3; i < 9; i++) {
 				if (Interfaces.get(25, i) != null
 						&& Interfaces.get(25, i).getModelID() == (getPuzzle() - 3)) {
 					return Interfaces.get(25, i);
