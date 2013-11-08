a98c8218cfcee21ae934236fb072dde463aef737
Tyler Storm <wussupscripts@gmail.com>
2013-11-08 17:14:53 -0500

    version update

--- types/Var.java
+++ types/Var.java
@@ -138,7 +138,7 @@ public class Var {
 	
 	public static boolean trial = false;
 
-	public static double version = 1.2;
+	public static double version = 1.21;
 
 	public static Timer runTime = new Timer(0);
 	
