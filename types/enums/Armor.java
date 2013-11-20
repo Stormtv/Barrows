82c0f5b85ef35bfc34ba785ad1eb15ebbb7189b6
Ruben <ruben.amendoeira@gmail.com>
2013-11-20 20:15:32 +0000

    THERE YOU GO CRYBABY

--- types/enums/Armor.java
+++ types/enums/Armor.java
@@ -6,47 +6,35 @@ import scripts.Barrows.types.enums.Equipment;
 
 public class Armor {
 	public enum Degradables {
-		GuthanHelm(new int[] { 4724, 4904, 4905, 4906, 4907 },
-				Equipment.Gear.HELM, 4908), GuthanLegs(new int[] { 4730, 4922,
-				4923, 4924, 4925 }, Equipment.Gear.LEGS, 4926), GuthanBody(
-				new int[] { 4728, 4916, 4917, 4918, 4919 },
-				Equipment.Gear.BODY, 4920), GuthanWarspear(new int[] { 4726,
-				4910, 4911, 4912, 4913 }, Equipment.Gear.WEAPON, 4914),
+		GuthanHelm(new int[] { 4724, 4904, 4905, 4906, 4907 }, Equipment.Gear.HELM, 4908),
+		GuthanLegs(new int[] { 4730, 4922, 4923, 4924, 4925 }, Equipment.Gear.LEGS, 4926),
+		GuthanBody(new int[] { 4728, 4916, 4917, 4918, 4919 }, Equipment.Gear.BODY, 4920),
+		GuthanWarspear(new int[] { 4726, 4910, 4911, 4912, 4913 }, Equipment.Gear.WEAPON, 4914),
 
-		DharoksGreatAxe(new int[] { 4718, 4886, 4887, 4888, 4889 },
-				Equipment.Gear.WEAPON, 4890), DharoksBody(new int[] { 4720,
-				4892, 4893, 4894, 4895 }, Equipment.Gear.BODY, 4896), DharoksLegs(
-				new int[] { 4722, 4898, 4899, 4900, 4901 },
-				Equipment.Gear.LEGS, 4902), DharoksHelm(new int[] { 4716, 4880,
-				4881, 4882, 4883 }, Equipment.Gear.HELM, 4884),
+		DharoksGreatAxe(new int[] { 4718, 4886, 4887, 4888, 4889 }, Equipment.Gear.WEAPON, 4890),
+		DharoksBody(new int[] { 4720, 4892, 4893, 4894, 4895 }, Equipment.Gear.BODY, 4896),
+		DharoksLegs(new int[] { 4722, 4898, 4899, 4900, 4901 }, Equipment.Gear.LEGS, 4902),
+		DharoksHelm(new int[] { 4716, 4880, 4881, 4882, 4883 }, Equipment.Gear.HELM, 4884),
 
-		VeracsHelm(new int[] { 4753, 4976, 4977, 4978, 4979 },
-				Equipment.Gear.HELM, 4981), VeracsBody(new int[] { 4757, 4988,
-				4989, 4990, 4991 }, Equipment.Gear.BODY, 4992), VeracsLegs(
-				new int[] { 4759, 4994, 4995, 4996, 4997 },
-				Equipment.Gear.LEGS, 4998), VeracsFlail(new int[] { 4755, 4982,
-				4983, 4984, 4985 }, Equipment.Gear.WEAPON, 4986),
+		VeracsHelm(new int[] { 4753, 4976, 4977, 4978, 4979 }, Equipment.Gear.HELM, 4981),
+		VeracsBody(new int[] { 4757, 4988, 4989, 4990, 4991 }, Equipment.Gear.BODY, 4992),
+		VeracsLegs(new int[] { 4759, 4994, 4995, 4996, 4997 }, Equipment.Gear.LEGS, 4998),
+		VeracsFlail(new int[] { 4755, 4982, 4983, 4984, 4985 }, Equipment.Gear.WEAPON, 4986),
 
-		KarilsBody(new int[] { 4736, 4940, 4941, 4942, 4943 },
-				Equipment.Gear.BODY, 4944), KarilsHelm(new int[] { 4732, 4982,
-				4929, 4930, 4931, 4932 }, Equipment.Gear.HELM, 4932), KarilsLegs(
-				new int[] { 4738, 4946, 4948, 4949, 4950 },
-				Equipment.Gear.LEGS, 4951), KarilsCBow(new int[] { 4734, 4934,
-				4935, 4936, 4937 }, Equipment.Gear.WEAPON, 4938),
+		KarilsBody(new int[] { 4736, 4940, 4941, 4942, 4943 }, Equipment.Gear.BODY, 4944),
+		KarilsHelm(new int[] { 4732, 4982, 4929, 4930, 4931, 4932 }, Equipment.Gear.HELM, 4932),
+		KarilsLegs(new int[] { 4738, 4946, 4948, 4949, 4950 }, Equipment.Gear.LEGS, 4951),
+		KarilsCBow(new int[] { 4734, 4934, 4935, 4936, 4937 }, Equipment.Gear.WEAPON, 4938),
 
-		ToragHelm(new int[] { 4745, 4952, 4953, 4954, 4955 },
-				Equipment.Gear.HELM, 4956), ToragBody(new int[] { 4749, 4964,
-				4965, 4966, 4967 }, Equipment.Gear.BODY, 4968), ToragLegs(
-				new int[] { 4751, 4970, 4971, 4972, 4973 },
-				Equipment.Gear.LEGS, 4974), ToragHammers(new int[] { 4747,
-				4958, 4959, 4960, 4961 }, Equipment.Gear.WEAPON, 4962),
+		ToragHelm(new int[] { 4745, 4952, 4953, 4954, 4955 }, Equipment.Gear.HELM, 4956),
+		ToragBody(new int[] { 4749, 4964, 4965, 4966, 4967 }, Equipment.Gear.BODY, 4968),
+		ToragLegs(new int[] { 4751, 4970, 4971, 4972, 4973 }, Equipment.Gear.LEGS, 4974),
+		ToragHammers(new int[] { 4747, 4958, 4959, 4960, 4961 }, Equipment.Gear.WEAPON, 4962),
 
-		AhrimHelm(new int[] { 4708, 4856, 4857, 4858, 4859 },
-				Equipment.Gear.HELM, 4860), AhrimBody(new int[] { 4712, 4868,
-				4869, 4870, 4871 }, Equipment.Gear.BODY, 4872), AhrimLeg(
-				new int[] { 4714, 4874, 4875, 4876, 4877 },
-				Equipment.Gear.LEGS, 4878), AhrimStaff(new int[] { 4710, 4862,
-				4863, 4864, 4865 }, Equipment.Gear.WEAPON, 4866);
+		AhrimHelm(new int[] { 4708, 4856, 4857, 4858, 4859 }, Equipment.Gear.HELM, 4860),
+		AhrimBody(new int[] { 4712, 4868, 4869, 4870, 4871 }, Equipment.Gear.BODY, 4872),
+		AhrimLeg(new int[] { 4714, 4874, 4875, 4876, 4877 }, Equipment.Gear.LEGS, 4878),
+		AhrimStaff(new int[] { 4710, 4862, 4863, 4864, 4865 }, Equipment.Gear.WEAPON, 4866);
 
 		private int[] id;
 		private Equipment.Gear gear;
--- types/enums/Magic.java
+++ types/enums/Magic.java
@@ -19,39 +19,44 @@ import scripts.Barrows.methods.BankHandler;
 import scripts.Barrows.types.Brother.Brothers;
 
 public class Magic {
-
 	public enum Spell {
-		NONE(-1, -1, -1, "None", -1, null), FIRE_BOLT(35, 20, 17, "Fire Bolt",
-				8, new int[][] { { 554, 4 }, { 556, 2 }, { 562, 1 } }), WIND_BLAST(
-				41, 24, 19, "Wind Blast", 9, new int[][] { { 556, 3 },
-						{ 560, 1 } }), WATER_BLAST(47, 27, 21, "Water Blast",
-				10, new int[][] { { 555, 3 }, { 556, 3 }, { 560, 1 } }), EARTH_BLAST(
-				53, 33, 23, "Earth Blast", 11, new int[][] { { 557, 4 },
-						{ 556, 3 }, { 560, 1 } }), FIRE_BLAST(59, 38, 25,
-				"Fire Blast", 12, new int[][] { { 554, 5 }, { 556, 3 },
-						{ 560, 1 } }), WIND_WAVE(62, 45, 27, "Wind Wave", 13,
-				new int[][] { { 556, 5 }, { 565, 1 } }), WATER_WAVE(65, 48, 29,
-				"Water Wave", 14, new int[][] { { 553, 7 }, { 556, 5 },
-						{ 565, 1 } }), EARTH_WAVE(70, 52, 31, "Earth Wave", 15,
-				new int[][] { { 557, 7 }, { 556, 5 }, { 565, 1 } }), FIRE_WAVE(
-				75, 55, 33, "Fire Wave", 16, new int[][] { { 554, 7 },
-						{ 556, 5 }, { 565, 1 } }), MAGIC_DART(50, 31, 37,
-				"Magic Dart", 18, new int[][] { { 560, 1 }, { 558, 4 } }), SMOKE_RUSH(
-				50, 8, 63, "Smoke Rush", 31, new int[][] { { 562, 2 },
-						{ 560, 2 }, { 554, 1 }, { 556, 1 } }), SHADOW_RUSH(52,
-				12, 65, "Shadow Rush", 32, new int[][] { { 562, 2 },
-						{ 560, 2 }, { 556, 1 }, { 566, 1 } }), BLOOD_RUSH(56,
-				4, 67, "Blood Rush", 33, new int[][] { { 562, 2 }, { 560, 2 },
-						{ 565, 1 } }), ICE_RUSH(58, 0, 69, "Ice Rush", 34,
-				new int[][] { { 562, 2 }, { 560, 2 }, { 555, 2 } }), SMOKE_BLITZ(
-				74, 9, 79, "Smoke Blitz", 39, new int[][] { { 565, 2 },
-						{ 560, 2 }, { 554, 2 }, { 556, 2 } }), SHADOW_BLITZ(76,
-				13, 81, "Shadow Blitz", 40, new int[][] { { 565, 2 },
-						{ 560, 2 }, { 556, 2 }, { 566, 2 } }), BLOOD_BLITZ(80,
-				5, 83, "Blood Blitz", 41,
-				new int[][] { { 565, 4 }, { 560, 2 } }), ICE_BLITZ(82, 1, 85,
-				"Ice Blitz", 42, new int[][] { { 565, 2 }, { 560, 2 },
-						{ 555, 3 } });
+		NONE(-1, -1, -1, "None", -1, null),
+		FIRE_BOLT(35, 20, 17, "Fire Bolt", 8, new int[][] { { 554, 4 },
+				{ 556, 2 }, { 562, 1 } }),
+		WIND_BLAST(41, 24, 19, "Wind Blast", 9, new int[][] { { 556, 3 },
+				{ 560, 1 } }),
+		WATER_BLAST(47, 27, 21, "Water Blast", 10, new int[][] { { 555, 3 },
+				{ 556, 3 }, { 560, 1 } }),
+		EARTH_BLAST(53, 33, 23, "Earth Blast", 11, new int[][] { { 557, 4 },
+				{ 556, 3 }, { 560, 1 } }),
+		FIRE_BLAST(59, 38, 25, "Fire Blast", 12, new int[][] { { 554, 5 },
+				{ 556, 3 }, { 560, 1 } }),
+		WIND_WAVE(62, 45, 27, "Wind Wave", 13, new int[][] { { 556, 5 },
+				{ 565, 1 } }),
+		WATER_WAVE(65, 48, 29, "Water Wave", 14, new int[][] { { 553, 7 },
+				{ 556, 5 }, { 565, 1 } }),
+		EARTH_WAVE(70, 52, 31, "Earth Wave", 15, new int[][] { { 557, 7 },
+				{ 556, 5 }, { 565, 1 } }),
+		FIRE_WAVE(75, 55, 33, "Fire Wave", 16, new int[][] { { 554, 7 },
+				{ 556, 5 }, { 565, 1 } }),
+		MAGIC_DART(50, 31, 37, "Magic Dart", 18, new int[][] { { 560, 1 },
+				{ 558, 4 } }),
+		SMOKE_RUSH(50, 8, 63, "Smoke Rush", 31, new int[][] { { 562, 2 },
+				{ 560, 2 }, { 554, 1 }, { 556, 1 } }),
+		SHADOW_RUSH(52, 12, 65, "Shadow Rush", 32, new int[][] { { 562, 2 },
+				{ 560, 2 }, { 556, 1 }, { 566, 1 } }),
+		BLOOD_RUSH(56, 4, 67, "Blood Rush", 33, new int[][] { { 562, 2 },
+				{ 560, 2 }, { 565, 1 } }),
+		ICE_RUSH(58, 0, 69, "Ice Rush", 34, new int[][] { { 562, 2 },
+				{ 560, 2 }, { 555, 2 } }),
+		SMOKE_BLITZ(74, 9, 79, "Smoke Blitz", 39, new int[][] { { 565, 2 },
+				{ 560, 2 }, { 554, 2 }, { 556, 2 } }),
+		SHADOW_BLITZ(76, 13, 81, "Shadow Blitz", 40, new int[][] { { 565, 2 },
+				{ 560, 2 }, { 556, 2 }, { 566, 2 } }),
+		BLOOD_BLITZ(80, 5, 83, "Blood Blitz", 41, new int[][] { { 565, 4 },
+				{ 560, 2 } }),
+		ICE_BLITZ(82, 1, 85, "Ice Blitz", 42, new int[][] { { 565, 2 },
+				{ 560, 2 }, { 555, 3 } });
 
 		private final int req;
 		private final int interfaceID;
